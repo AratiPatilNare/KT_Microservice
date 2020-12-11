c0107d368fcb64a9a8b07802a7ce5fb564b6e88d
ARATIPATIL <ARATIPATIL@48.240.33.248>
2020-12-11 14:09:07 +0530

    commiting

---
+++ authserver/src/main/java/com/example/authserver/dao/OAuthDAOService.java
@@ -0,0 +1,12 @@
+package com.example.authserver.dao;
+
+import com.example.authserver.model.UserEntity;
+
+public interface OAuthDAOService {
+
+	public UserEntity getUserDetails(String emailId);
+	
+}
+
+
+
